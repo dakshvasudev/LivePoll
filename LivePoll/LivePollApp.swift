12397b5fd08fe81a6fda89965cc5e2f6b5fa9096
dakshvasudev <91746245+dakshvasudev@users.noreply.github.com>
2024-06-03 23:28:31 +0530

    Initial Commit

---
+++ LivePoll/LivePollApp.swift
@@ -0,0 +1,17 @@
+//
+//  LivePollApp.swift
+//  LivePoll
+//
+//  Created by daksh vasudev on 03/06/24.
+//
+
+import SwiftUI
+
+@main
+struct LivePollApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
